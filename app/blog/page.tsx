2851424459c64989821b7a192467b2551c0453d3
SlavaHeorh <slava.georg.k@gmail.com>
2023-06-08 11:52:31 +0300

    first lesson done

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,6 +4,9 @@ import Link from "next/link";
 async function getData() {
     // @ts-ignore
     const response = await fetch('https://jsonplaceholder.typicode.com/posts', { next: { revalidate: 60 } });
+
+    if (!response.ok) throw new Error('Unable to fetch posts!')
+
     return response.json()
 }
 
