1aa8985b09031f2d8d37032c07efafb9b34a4752
SlavaHeorh <slava.georg.k@gmail.com>
2023-06-21 15:23:48 +0300

    get-post-postman

---
+++ app/api/posts/posts.ts
@@ -0,0 +1,17 @@
+export const posts = [
+    {
+        id: 1,
+        title: 'Test post ONE',
+        body: 'field body ONE'
+    },
+    {
+        id: 2,
+        title: 'Test post TWO',
+        body: 'field body TWO'
+    },
+    {
+        id: 3,
+        title: 'Test post THREE',
+        body: 'field body THREE'
+    }
+]
