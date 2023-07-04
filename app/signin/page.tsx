81e5fd5a2ac9d3987ace78fcd2fbfc23185037ed
SlavaHeorh <slava.georg.k@gmail.com>
2023-07-04 14:32:23 +0300

    add some changes

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,7 +1,8 @@
 import { GoogleButton } from "@/components/GoogleButton";
-import { SignInForm } from "@/components/SigninForm";
+import { SignInForm } from "@/components/SignInForm";
 
 export default async function Signin() {
+
     return (
         <div className="stack">
             <h1>SignIn</h1>
